3a599d793daf36c1bd5cb62e69fbc1767878598e
weffology <stephlarkinrose@gmail.com>
2019-09-17 23:47:24 -0400

    made improvements after class tonight, but still missing key pieces of functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function() {
     
     //set global variables
     var timeLeft = 60;
     var score = 0;
-    var userAnswers = [];
-    var correctAnswers = ["B", "C", "A"];
+    
+    var questions = [{
+        question: 'Where does Veronica live?',
+        options: ['Newport Beach', 'Neptune', 'Huntington Beach'],
+        answer: 'Neptune'
+    }, {
+        question: 'What does a trip to the dentist mean?',
+        options: ['Going to the dentist', 'Going to jail', 'Someone spitting in your drink'],
+        answer: 'Someone spitting in your drink'
+    }, {
+        question: 'What do Veronica Mars fans call themselves?',
+        options: ['Marshmallows', 'Martians', 'Sleuth Sistas'],
+        answer: 'Marshmallows'
+    }];
 
     //create countdown timer function
     function triviaStart() {
@@ -19,31 +31,40 @@ $(document).ready(function(){
                 $("#countdown").html("Score: "+ score);
             }
         }
-    }
+    };
 
-    function scoreTally() {
+    //create a function that builds out the questions
+    function questionBuildOut() {
+        for (i=0; i<=questions.length; i++) {
 
-    }
+            var qDivCreate = "<div class='quiz-questions'>" + questions[i].question + "</div>";
 
-    //create function to check that only one answer is selected for each question
-    function oneAnswerEach() {
-        var options = $("input");
-        for (var i=0; i<options.length; i++) {
-            if (options[i].checked) {
-                return true;
+            $("#quiz").append(qDivCreate);
+
+            for (o=0; o<=2; o++) {
+                
+                //having trouble here with getting only one radio per question...
+                //var qName = "question" + //what goes here? i've tried so many things...;
+                var qOptionsCreate = "<label><input type='radio' name='qName'>" + questions[i].options[o] + "</label>";
+                
+                $("#quiz").append(qOptionsCreate);
             }
         }
-        return false;
     }
 
     //create on click function for start button
     $("#start-button").click(function(){
-        $("#quiz-questions").removeClass("hide");
         $("#countdown").html("Time Remaining: " + parseInt(timeLeft));
         triviaStart();
-    })
-
-    //create function that tracks answers when clicked
-    $("input").click(function(){
+        questionBuildOut();
+        $("#submit").removeClass("hide").addClass("button");
     })
 })
+    //     //Question 1 - Correct Answer is B
+    //     "<p>Where does Veronica live?</p><label><input type='radio' name='question1' value='A'> Newport Beach</label><label><input type='radio' name='question1' value='B'> Neptune</label><label><input class='q1' type='radio' name='question1' value='C'> Huntington Beach</label>", 
+        
+    //     //Question 2 - Correct Answer is C
+    //     "<p>What does a trip to the dentist mean?</p><label><input type='radio' name='question2' value='A'> Going to the dentist</label><label><input type='radio' name='question2' value='B'> Going to jail</label><label><input type='radio' name='question2' value='C'> Someone spitting in your drink</label>", 
+        
+    //     //Question 3 - Correct Answer is A
+    //     "<p>What do fans of Veronica Mars call themselves?</p><label><input type='radio' name='question3' value='A'> Marshmallows</label><label><input type='radio' name='question3' value='B'> Martians</label><label><input type='radio' name='question3' value='C'> Sleuth Sisters</label>"]
